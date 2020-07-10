bef881c97520a567ba6cfab349bf108d4cd5e4f8
Alexander Donets <xander.donets@yandex.ru>
2020-07-11 04:15:29 +0700

    Small cleaning. Added map loading method to GameWorld

--- labyrinth/entities/world.py
+++ labyrinth/entities/world.py
@@ -1,5 +1,6 @@
 # Nests data of game world
 
+import os
 import json
 import random
 
@@ -8,12 +9,15 @@ from entities.enum import CellType
 
 class GameWorld:
     def __init__(self, config):
-        self.cells = [[CellType.REGULAR.value for x in range(self.w)] for y in range(self.h)]
         self.config = config
         self.w = config['width']
         self.h = config['height']
 
         random.seed(42)
+
+        self.cells = [[CellType.REGULAR.value for x in range(self.w)] for y in range(self.h)]
+
+        self.load_map(f'{os.getcwd()}/saves/test_map.json')
 
     # TODO: Generate Labyrinth
     def generate_map(self):
@@ -22,4 +26,3 @@ class GameWorld:
     def load_map(self, file_path):
         with open(file_path) as map_file:
             self.map = json.load(map_file)
-
